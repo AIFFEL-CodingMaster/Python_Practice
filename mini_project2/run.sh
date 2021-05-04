a3ffce39653638873c1ee69a4e0ee7c68813fb5d
Jungminchae <minchae3618@gmail.com>
2021-05-04 11:42:56 +0900

    fix requirement and add run

--- mini_project2/run.sh
+++ mini_project2/run.sh
@@ -1,11 +1,6 @@
-# 가상환경 생성
-conda env create -f env.yaml
-# 가상환경 실행
-conda activate mini_project
 # 패키지 설치
 pip install -r requirements.txt
 # django command
 python manage.py migrate
 # runserver
 python manage.py runserver
-
